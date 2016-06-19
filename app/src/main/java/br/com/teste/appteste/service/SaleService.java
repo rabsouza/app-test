5885ae508d24c6b0efb55c96be85e5e121a611c5
Rafael Batista <rabsouza@gmail.com>
2016-06-19 19:39:48 -0300

    [rabsouza] Ordenando a lista de vendas

--- app/src/main/java/br/com/teste/appteste/service/SaleService.java
+++ app/src/main/java/br/com/teste/appteste/service/SaleService.java
@@ -1,5 +1,9 @@
 package br.com.teste.appteste.service;
 
+import com.google.common.collect.ComparisonChain;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import br.com.teste.appteste.domain.Sale;
@@ -11,7 +15,17 @@ import br.com.teste.appteste.mock.MockSale;
 
 public class SaleService {
 
+    private Comparator<Sale> saleComparator = new Comparator<Sale>() {
+        @Override
+        public int compare(Sale lhs, Sale rhs) {
+            return ComparisonChain.start().compare(lhs.getDate(), rhs.getDate()).result();
+        }
+    };
+
     public List<Sale> findSalesByUser(String user) {
-        return MockSale.createDataMock();
+        List<Sale> sales = MockSale.createDataMock();
+        Collections.sort(sales, saleComparator);
+        Collections.reverse(sales);
+        return sales;
     }
 }
